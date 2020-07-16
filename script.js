b1241369361c7a71f0addb4b519c4df0e89e9e32
Glitch (hello-webpage) <none>
2020-07-16 15:43:14 +0000

    🌷⛰ Checkpoint ./script.js:11113072/3490

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var links = document.links;
 for (var num = 0; num < links.length; num++) {
-  if (links[num].href.indexOf(location.pathname) >= 0 /*&& links[num].onclick != null*/) {
+  if (
+    links[num].href.indexOf(location.pathname) >=
+    0 /*&& links[num].onclick != null*/
+  ) {
     links[num].onclick = function(e) {
-      alert(this.href);
+      var link = this.href;
       e.preventDefault();
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+          window.history.pushState({ path: link }, "", link);
           document.open();
           document.write(this.responseText);
         }
