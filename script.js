bab7cac8ff4535424266aea9a433a823103c2182
Glitch (hello-webpage) <none>
2020-07-16 15:54:38 +0000

    🏮🗺 Checkpoint ./script.js:11113072/11593

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 var links = document.links;
+var link;
+function write() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      window.history.pushState({}, "", link);
+      document.open();
+      document.write(this.responseText);
+    }
+  };
+  xhttp.open("GET", this.href, true);
+  xhttp.send();
+}
 for (var num = 0; num < links.length; num++) {
   if (
     links[num].href.indexOf(location.pathname) >=
     0 /*&& links[num].onclick != null*/
   ) {
     links[num].onclick = function(e) {
-      var link = this.href;
+      link = this.href;
       e.preventDefault();
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          window.history.pushState({ path: link }, "", link);
-          document.open();
-          document.write(this.responseText);
-        }
-      };
-      xhttp.open("GET", this.href, true);
-      xhttp.send();
     };
   }
 }
+window.onpopstate = function() {
+  alert("popped!");
+};
