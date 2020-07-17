405a6089921197aa48f5fc1b46ac6ab956edfe18
Glitch (hello-webpage) <none>
2020-07-17 12:34:24 +0000

    🌎🌨 Checkpoint ./deload.js:11113072/1836

--- deload.js
+++ deload.js
@@ -1,34 +1,37 @@
-var xhttp;
-var links = document.links;
-function write(link) {
-  if (window.XMLHttpRequest) {
-    xhttp = new XMLHttpRequest();
-  } else {
-    location.href = link;
-  }
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.open();
-      document.write(this.responseText);
-      document.close();
+(function() {
+  "use strict";
+  var xhttp;
+  var links = document.links;
+  function write(link) {
+    if (window.XMLHttpRequest) {
+      xhttp = new XMLHttpRequest();
+    } else {
+      location.href = link;
     }
-  };
-  xhttp.open("GET", link, true);
-  xhttp.send();
-}
-for (var num = 0; num < links.length; num++) {
-  if (
-    links[num].href.indexOf(location.pathname) >= 0 &&
-    links[num].href != null &&
-    links[num].onclick == null
-  ) {
-    links[num].onclick = function(e) {
-      e.preventDefault();
-      window.history.pushState({ path: this.href }, "", this.href);
-      write(this.href);
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.open();
+        document.write(this.responseText);
+        document.close();
+      }
     };
+    xhttp.open("GET", link, true);
+    xhttp.send();
   }
-}
-window.onpopstate = function() {
-  write(location.pathname);
-};
+  for (var num = 0; num < links.length; num++) {
+    if (
+      links[num].href.indexOf(location.pathname) >= 0 &&
+      links[num].href != null &&
+      links[num].onclick == null
+    ) {
+      links[num].onclick = function(e) {
+        e.preventDefault();
+        window.history.pushState({ path: this.href }, "", this.href);
+        write(this.href);
+      };
+    }
+  }
+  window.onpopstate = function() {
+    write(location.pathname);
+  };
+});
