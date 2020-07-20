6d2dc6205d79bbead214cb63707cd093be07a5ff
Glitch (deload) <none>
2020-07-20 21:56:50 +0000

    🚆😹 Checkpoint ./defresh.js:11113072/7028:19215956/870 ./chat.txt:11113072/393:19215956/850 ./demo/index.html:11113072/22191:19215956/65

--- defresh.js
+++ defresh.js
@@ -17,10 +17,7 @@ function defresh(r, a) {
     x = new d.ActiveXObject("Microsoft.XMLHTTP");
   }
   x.onreadystatechange = function() {
-    if (
-      this.readyState == 4 &&
-      this.responseText.indexOf("defresh" + unescape("%2E%6A%73")) >= 0
-    ) {
+    if (this.readyState == 4 && this.responseText.indexOf("defresh.js") >= 0) {
       if (a.toLowerCase() == "push" && window.history) {
         w.history.pushState({ page: r }, "", r);
       }
@@ -31,10 +28,7 @@ function defresh(r, a) {
       d.write(this.responseText);
       d.close();
     }
-    if (
-      this.readyState == 4 &&
-      this.responseText.indexOf("defresh" + unescape("%2E%6A%73")) < 0
-    ) {
+    if (this.readyState == 4 && this.responseText.indexOf("defresh.js") < 0) {
       if (a.toLowerCase() == "replace") {
         w.location.replace(r);
       } else {
@@ -51,7 +45,8 @@ for (var i = 0; i < l.length; i++) {
     l[i].href != null &&
     l[i].onclick == null &&
     l[i].target != "_blank" &&
-    l[i].target != "_parent"
+    l[i].target != "_parent" &&
+    l[i].href.indexOf(".js") < 0
   ) {
     l[i].onclick = function(e) {
       if (p["Control"] != true && p["Shift"] != true) {
