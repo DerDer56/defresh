1f128d51f1a834f7535ac94e504f7c7710f0957c
Glitch (deload) <none>
2020-07-20 23:06:50 +0000

    ⛈👨 Checkpoint ./demo/script.js:11113072/2998 ./demo/more.html:11113072/8637 ./style.css:11113072/38

--- demo/script.js
+++ demo/script.js
@@ -1,4 +1,17 @@
-var defresh
+var defresh;
+var input = document.getElementById("input");
+input.onkeydown = function(e) {
+  if (e.key == "Enter") {
+    enter();
+  }
+};
 function enter() {
-  
+  var x = input.value.toLowerCase();
+  if (x == "pepperoni") {
+    defresh("./pepperoni.html", "push");
+  } else if (x == "cheese") {
+    defresh("./cheese.html", "push");
+  } else {
+    defresh("/", "push");
+  }
 }
